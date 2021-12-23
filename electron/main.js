35f5555acfe6f70d690aad2845bdae2ddb59e1d8
Erwin Hessing <ejhessing@gmail.com>
2021-12-23 16:36:19 +1300

    working

--- electron/main.js
+++ electron/main.js
@@ -2,6 +2,12 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
 const { channels } = require('../src/shared/constants');
+const { autoUpdater } = require("electron-updater");
+const log = require('electron-log');
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'silly';
+
 
 let mainWindow;
 
@@ -16,6 +22,7 @@ function createWindow () {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
     },
   });
   mainWindow.loadURL(startUrl);
@@ -24,7 +31,11 @@ function createWindow () {
   });
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+  autoUpdater.checkForUpdatesAndNotify();
+
+});
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
@@ -44,3 +55,33 @@ ipcMain.on(channels.APP_INFO, (event) => {
     appVersion: app.getVersion(),
   });
 });
+
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+    mainWindow.webContents.send('restartRequired');
+    autoUpdater.quitAndInstall();
+});
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send('message', {text});
+}
--- src/react/App.js
+++ src/react/App.js
@@ -10,22 +10,33 @@ class App extends Component {
     this.state = {
       appName: '',
       appVersion: '',
+      messages: []
     }
     ipcRenderer.send(channels.APP_INFO);
     ipcRenderer.on(channels.APP_INFO, (event, arg) => {
       ipcRenderer.removeAllListeners(channels.APP_INFO);
       const { appName, appVersion } = arg;
-      this.setState({ appName, appVersion });
+      this.setState({ ...this.state, appName, appVersion });
+    });
+    ipcRenderer.on(channels.MESSAGE, (event, arg) => {
+      const { text } = arg;
+      console.log('**', { event, arg })
+      this.setState({ ...this.state, messages: [...this.state.messages, text] });
     });
   }
 
   render() {
-    const { appName, appVersion } = this.state;
+    console.log('inside react app')
+    const { appName, appVersion, messages } = this.state;
+    console.log({ messages })
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{appName} version {appVersion}</p>
+          {messages.map(text => {
+            return (<p key={text}>{text}</p>)
+          })}
         </header>
       </div>
     );
